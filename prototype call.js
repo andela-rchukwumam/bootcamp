21dd02cf6b162c87826988de3b56874d49c83326
ruthchukwumam <aruth71@gmail.com>
2015-07-07 17:42:05 +0100

    prototype call.js

---
+++ prototype call.js
@@ -0,0 +1,8 @@
+var students = ["Lovelyn", "Ibrahim", "Andrew"];
+Array.prototype.bootcamp = function (){
+for (i = 0; i < this.length; i++){
+	document.write(this[i] + " is in bootcamp." + "<br>");
+	}
+}
+students.bootcamp();
+
